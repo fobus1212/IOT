a44ef185f9cc8ef1805461374dcf9e7364eec0b6
Trofimov Andrey <fobus995511@gmail.com>
2023-09-07 12:37:23 +0300

    )))

--- praktick/simpll_less.py
+++ praktick/simpll_less.py
@@ -1,14 +1,21 @@
 n=int(input()) 
 
 def guess(n): 
-    if n==1: 
-        b=1 
-    else  :   
-        for i in range(n): 
-            if i*i== n :  
-                b=i 
-            i+=1 
-    return b 
+    if n/2<1: 
+        b=(n) 
+    elif n>0 :   
+        for i in range(n+1): 
+            if i*i== n and i!=n :  
+                b=i  
+                i+=1  
+                if b is not None: 
+                    return b 
+            elif i==n: 
+                b="Сложно , сложно , не могу"
+
+                
+    return b  
+    
  
 a=guess(n)  
 print(a)
