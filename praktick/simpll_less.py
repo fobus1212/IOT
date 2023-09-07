fd80a8c85c9201cedb64482ab920237b1245bdd8
Trofimov Andrey <fobus995511@gmail.com>
2023-09-07 11:17:31 +0300

    практика3 решеная задача

---
+++ praktick/simpll_less.py
@@ -0,0 +1,21 @@
+n=int(input()) 
+
+def guess(n): 
+    if n==1: 
+        b=1 
+    else  :   
+        for i in range(n): 
+            if i*i== n :  
+                b=i 
+            i+=1 
+    return b 
+ 
+a=guess(n)  
+print(a)
+
+                        
+
+
+
+ 
+
